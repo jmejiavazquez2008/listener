1a5601443c7a9fb3eb0d980d4a8b432027e412eb
jmejiavazquez2008 <44645252+jmejiavazquez2008@users.noreply.github.com>
2019-07-23 14:53:19 -0400

    Update outboundmsg.js

--- routes/outboundmsg.js
+++ routes/outboundmsg.js
@@ -31,7 +31,7 @@ unwrapMessage = function(obj) {
     for (i = 0; i < numNotifications; i++)
     {
       sobjects.push(obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[i].sobject[0]);
-       console.log(obj);
+       //console.log(obj);
        console.log(obj['soapenv:envelope']['soapenv:body'][0]);
        console.log(obj['soapenv:envelope']['soapenv:body'][0].notifications[0]);
        console.log(obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[i].sobject[0]);
